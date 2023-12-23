790604051a912980ce6ccf9d93a674bdc389b1d0
miguelfrndz <miguel.fretana@gmail.com>
2023-12-23 17:32:14 +0100

    Added Tree Plugin

--- init.lua
+++ init.lua
@@ -29,10 +29,16 @@ local plugins = {
         'nvim-telescope/telescope.nvim', tag = '0.1.5',
         dependencies = { 'nvim-lua/plenary.nvim' }
     },
+    {
+        "nvim-tree/nvim-tree.lua", version = "*",
+        lazy = false,
+    }
 }
 local opts = {}
 
 require("lazy").setup(plugins, opts)
+
+local nvim_tree = require("nvim-tree").setup()
 
 local builtin = require("telescope.builtin")
 
