534ec9a784662d82f022e2107c595e630a691c39
miguelfrndz <miguel.fretana@gmail.com>
2024-01-17 20:16:26 +0100

    Added LSP Support

--- init.lua
+++ init.lua
@@ -32,7 +32,71 @@ local plugins = {
     {
         "nvim-tree/nvim-tree.lua", version = "*",
         lazy = false,
-    }
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        cmd = 'Mason',
+        branch = 'v2.x',
+        dependencies = {
+            { 'neovim/nvim-lspconfig' },
+            {
+                'williamboman/mason.nvim',
+                build = function()
+                    pcall(vim.cmd, 'MasonUpdate')
+                end
+            },
+            { 'williamboman/mason-lspconfig.nvim', },
+
+            { 'hrsh7th/nvim-cmp' },
+            { 'hrsh7th/cmp-nvim-lsp' },
+            { 'L3MON4D3/LuaSnip' },
+            { 'SmiteshP/nvim-navic' }
+        },
+        config = function()
+
+            local lsp = require('lsp-zero').preset({})
+
+            local navic = require('nvim-navic')
+
+            lsp.on_attach(function(client, bufnr)
+                lsp.default_keymaps({buffer = bufnr})
+                if client.server_capabilities.documentSymbolProvider then
+                    navic.attach(client, bufnr)
+                end
+            end)
+
+            require('lspconfig').lua_ls.setup(lsp.nvim_lua_ls())
+
+            lsp.setup()
+
+            local cmp = require('cmp')
+            -- local cmp_action = require('lsp-zero').cmp_action()
+
+            require('luasnip.loaders.from_vscode').lazy_load()
+
+            cmp.setup({
+                preselect = cmp.PreselectMode.None,
+                sources = {
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                },
+                mapping = {
+                    ['<CR>'] = cmp.mapping.confirm({ select = false }),
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    documentation = cmp.config.window.bordered(),
+                },
+                snippet = {
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body)
+                    end
+                }
+            })
+
+        end
+    },
 }
 local opts = {}
 
